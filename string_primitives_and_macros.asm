65dd54aba5247ca8c295439849a835d357d2ef78
ElliottLarsen <86161304+ElliottLarsen@users.noreply.github.com>
2021-12-12 18:44:47 -0600

    Initial commit

---
+++ string_primitives_and_macros.asm
@@ -0,0 +1,27 @@
+TITLE String Primitives and Macros     (string_primitives_and_macros.asm)
+
+; Author: Elliott Larsen
+; Date:
+; Description: 
+
+INCLUDE Irvine32.inc
+
+; (insert macro definitions here)
+
+; (insert constant definitions here)
+
+.data
+
+; (insert variable definitions here)
+
+.code
+main PROC
+
+; (insert executable instructions here)
+
+	Invoke ExitProcess,0	; exit to operating system
+main ENDP
+
+; (insert additional procedures here)
+
+END main
